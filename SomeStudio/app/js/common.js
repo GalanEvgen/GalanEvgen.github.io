c7f7f4858dff68fd66117aa61856a49336c124d8
GalanEvgen <e.galan@i.ua>
2017-03-28 16:10:04 +0300

    mmenu

--- SomeStudio/app/js/common.js
+++ SomeStudio/app/js/common.js
@@ -1,5 +1,20 @@
 $(function () {
 
-	// Custom JS
-
+	$("#my-menu").mmenu({
+		extensions:[ 'theme-dark', 'effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: '<img src ="img/favicon/logo.svg" alt="Фотостудия Pro Studio>"'
+		},
+		offCanvas: {
+            position: "right"
+         }
+	});
+	
+var api = $('#my-menu').data('mmenu');
+	api.bind('opened', function() {
+		$('.hamburger').addClass('is-active');
+	}).bind('closed', function() {
+					$('.hamburger').removeClass('is-active');
+		});
+	
 });
