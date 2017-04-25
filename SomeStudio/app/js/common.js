34b2555f3ac3027eef217abcf01fd167c029ebe3
GalanEvgen <e.galan@i.ua>
2017-04-25 16:51:30 +0300

    'caruselServise'

--- SomeStudio/app/js/common.js
+++ SomeStudio/app/js/common.js
@@ -10,18 +10,18 @@ $(function () {
          }
 	});
 	
-var api = $('#my-menu').data('mmenu');
+	var api = $('#my-menu').data('mmenu');
 	api.bind('opened', function() {
 		$('.hamburger').addClass('is-active');
 	}).bind('closed', function() {
-					$('.hamburger').removeClass('is-active');
-		});
+		$('.hamburger').removeClass('is-active');
+	});
 	
-$('.carousel-service').owlCarousel({
-	loop: true,
-	nav: true,
-	smartSpeed: 700,
-	responsiveClass: true,
+	$('.carousel-service').owlCarousel({
+		//loop: true,
+		nav: true,
+		smartSpeed: 700,
+		responsiveClass: true,
 		responsive: {
 			0: {
 				items: 1
@@ -33,9 +33,22 @@ $('.carousel-service').owlCarousel({
 				items: 3
 			}
 		},
+		
+		
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>']
+	});
 	
+	function carouselServise() {
+		$('.item-carousel-service').each(function() {
+			var ths = $(this),
+					thsh = ths.find('.carousel-service-content').outerHeight();
+					ths.find('.carousel-service-image').css('min-height', thsh);
+		});
+	}carouselServise();
 	
-	navText: ['<i class="fa fa-clock-o"></i>', '<i class="fa fa-clock-o"></i>']
-});
+	$('.carousel-service-composition .h2').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+	});
 	
 });
